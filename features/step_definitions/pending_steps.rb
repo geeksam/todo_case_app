c4d4063a206ddf910de33643a8c142bffd0b14c1
John Wilger <johnwilger@gmail.com>
2012-05-22 15:06:11 -0700

    Added pending step definitions for cukes

---
+++ features/step_definitions/pending_steps.rb
@@ -0,0 +1,83 @@
+When /^I "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I choose to create a new todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system asks me what I want to call the new todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I provide a name for the todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system shows the new todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I see that the todo list is currently empty$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have a todo list named "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I provide the name "(.*?)" for the todo list$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system tells me that I already have a todo list named "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system shows me the "(.*?)" todo list$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I choose to cancel out of creating a new todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system returns me to what I was doing before I chose to create a new todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have not created a todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I launch the application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system asks for my email address and password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I provide my correct email address and password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have most recently viewed the "(.*?)" todo list$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I provide and incorrect email or password$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system tells me it could not authenticate me$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the system does not display any todo list$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I launch the application by explicitely viewing the "(.*?)" todo list$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
